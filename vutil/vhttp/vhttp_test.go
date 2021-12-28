400f837b63cbd5d330e6df7878dc37c0b6064c71
zf1976 <1596863112@qq.com>
2021-12-29 01:04:05 +0800

    feat: update vutil url util

--- vutil/vhttp/vhttp_test.go
+++ vutil/vhttp/vhttp_test.go
@@ -2,8 +2,23 @@ package vhttp
 
 import (
 	"fmt"
+	"log"
 	"testing"
 )
+
+func TestURL(t *testing.T) {
+	a := IsURL("hi/there?")
+	log.Print(a)
+
+	b := IsURL("http://golang.cafe/")
+	log.Print(b)
+
+	c := IsURL("http://golang.org/index.html?#page1")
+	log.Print(c)
+
+	d := IsURL("golang.org")
+	log.Print(d)
+}
 
 func TestDomain(t *testing.T) {
 	domName := "www.golang.org"
