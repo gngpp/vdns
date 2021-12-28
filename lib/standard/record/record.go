08eb8bb5b245cc8e87bcf5997f196c3cbd115707
zf1976 <1596863112@qq.com>
2021-12-28 23:58:00 +0800

    feat: update record type enum

--- lib/standard/record/record.go
+++ lib/standard/record/record.go
@@ -4,16 +4,16 @@ type Type string
 
 //goland:noinspection ALL
 const (
-	a           Type = "A"     // 将域名指向一个IPV4地址
-	aaaa        Type = "AAAA"  // 将域名指向一个IPV6地址
-	ns          Type = "NS"    // 将子域名指定其他DNS服务器解析
-	mx          Type = "MX"    // 将域名指向邮件服务器地址
-	cname       Type = "CNAME" // 将域名指向另外一个域名
-	txt         Type = "TXT"   // 文本长度限制512，通常做SPF记录（反垃圾邮件）
-	srv         Type = "SRV"   // 记录提供特定的服务的服务器
-	ca          Type = "CA"    // CA证书颁发机构授权校验
-	explicitUrl Type = "显性URL" // 将域名重定向到另外一个地址
-	hiddenUrl   Type = "隐性URL" // 与显性URL类似，但是会隐藏真实目标地址
+	a            Type = "A"            // 将域名指向一个IPV4地址
+	aaaa         Type = "AAAA"         // 将域名指向一个IPV6地址
+	ns           Type = "NS"           // 将子域名指定其他DNS服务器解析
+	mx           Type = "MX"           // 将域名指向邮件服务器地址
+	cname        Type = "CNAME"        // 将域名指向另外一个域名
+	txt          Type = "TXT"          // 文本长度限制512，通常做SPF记录（反垃圾邮件）
+	srv          Type = "SRV"          // 记录提供特定的服务的服务器
+	ca           Type = "CA"           // CA证书颁发机构授权校验
+	redirect_url Type = "REDIRECT_URL" // 将域名重定向到另外一个地址
+	forward_url  Type = "FORWARD_URL"  // 与显性URL类似，但是会隐藏真实目标地址
 )
 
 var typeMap map[Type]Type
@@ -28,8 +28,8 @@ const (
 	TXT          = txt
 	SRV          = srv
 	CA           = ca
-	EXPLICIT_URL = explicitUrl
-	HIDDEN_URL   = hiddenUrl
+	REDIRECT_URL = redirect_url
+	FORWARD_URL  = forward_url
 )
 
 func OfType(t Type) (Type, bool) {
@@ -44,15 +44,15 @@ func Support(t Type) bool {
 
 func init() {
 	typeMap = map[Type]Type{
-		a:           A,
-		aaaa:        AAAA,
-		ns:          NS,
-		mx:          MX,
-		cname:       CNAME,
-		txt:         TXT,
-		srv:         SRV,
-		ca:          CA,
-		explicitUrl: EXPLICIT_URL,
-		hiddenUrl:   HIDDEN_URL,
+		a:            A,
+		aaaa:         AAAA,
+		ns:           NS,
+		mx:           MX,
+		cname:        CNAME,
+		txt:          TXT,
+		srv:          SRV,
+		ca:           CA,
+		redirect_url: REDIRECT_URL,
+		forward_url:  FORWARD_URL,
 	}
 }
