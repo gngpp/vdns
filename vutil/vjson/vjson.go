2c82fe3d42678d6ddcf305bb37fde318bba1e0d5
zf1976 <1596863112@qq.com>
2021-12-31 23:42:16 +0800

    feat: update lib vutil

--- vutil/vjson/vjson.go
+++ vutil/vjson/vjson.go
@@ -1,8 +1,62 @@
 package vjson
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+)
 
-func Prettify(i interface{}) string {
+func PrettifyString(i interface{}) string {
 	resp, _ := json.MarshalIndent(i, "", "   ")
 	return string(resp)
+}
+
+func Prettify(data interface{}) ([]byte, error) {
+	return json.MarshalIndent(data, "", "   ")
+}
+
+// Convert is use convert map[string]interface object to struct
+func Convert(in interface{}, out interface{}) error {
+	byt, _ := json.Marshal(in)
+	err := json.Unmarshal(byt, out)
+	return err
+}
+
+// Recover is use convert map[string]interface object to struct
+func Recover(in interface{}) error {
+	if in == nil {
+		return nil
+	}
+	return errors.New(fmt.Sprint(in))
+}
+
+func MapToJson(data map[string]interface{}) string {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return ""
+	}
+	return string(jsonData)
+}
+
+func StructToString(data interface{}) string {
+	jsonData, err := Prettify(data)
+	if err != nil {
+		return ""
+	}
+	return string(jsonData)
+}
+
+// ToMap JSON (json转map)
+func ToMap(data string) map[string]interface{} {
+	var jsonData map[string]interface{}
+	json.Unmarshal([]byte(data), &jsonData)
+	return jsonData
+}
+
+// ToIntArray
+// JSONstring (json转IntList)
+func ToIntArray(data string) []int {
+	var tmp = make([]int, 0)
+	json.Unmarshal([]byte(data), &tmp)
+	return tmp
 }
