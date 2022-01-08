d7614aa72b04fbe102add24befee8dc3c07348c8
zf1976 <1596863112@qq.com>
2022-01-08 17:07:18 +0800

    fix: Fixed the issue that "rare domain suffix" could not be extracted

--- vutil/vhttp/domain.go
+++ vutil/vhttp/domain.go
@@ -2,34 +2,43 @@ package vhttp
 
 import (
 	"errors"
-	"regexp"
 	"strings"
 	"vdns/vutil/strs"
 )
 
-var DomainRegexp = regexp.MustCompile(`^(([a-zA-Z]{1})|([a-zA-Z]{1}[a-zA-Z]{1})|([a-zA-Z]{1}[0-9]{1})|([0-9]{1}[a-zA-Z]{1})|([a-zA-Z0-9][a-zA-Z0-9-_]{1,61}[a-zA-Z0-9]))\.([a-zA-Z]{2,6}|[a-zA-Z0-9-]{2,30}\.[a-zA-Z]{2,3})$`)
+var rareDomainSuffix = []string{"com.cn", "org.cn", "net.cn", "ac.cn", "eu.org"}
 
-// ExtractDomain
+type Domain struct {
+	Domain    string
+	SubDomain string
+}
+
+// CheckExtractDomain
 // 提取顶级主域名跟域名记录
 // example：
 // www.baidu.com -> 顶级域名：baidu.com  记录：www
 // a.b.baidu.com -> 顶级域名：baidu.com   记录：a.b
-func ExtractDomain(domain string) ([]string, error) {
-	if strs.IsEmpty(domain) || !IsDomain(domain) {
-		return nil, errors.New("the domain name does not meet the specification")
-	}
+func CheckExtractDomain(domain string) (*Domain, error) {
 	split := strings.Split(domain, ".")
 	length := len(split)
-	mainDomain := strs.Concat(split[length-2], ".", split[length-1])
-	rr := ""
-	if length > 2 {
-		index := strings.LastIndex(domain, mainDomain)
-		rr = domain[:index-1]
+	if strs.IsEmpty(domain) || length <= 1 {
+		return nil, errors.New("the domain name does not meet the specification")
 	}
-	return []string{mainDomain, rr}, nil
-}
 
-// IsDomain Golang does not support Perl syntax ((?
-func IsDomain(domain string) bool {
-	return DomainRegexp.MatchString(domain)
+	targetDomain := new(Domain)
+	targetDomain.Domain = strs.Concat(split[length-2], ".", split[length-1])
+	// add special suffix domain name processing
+	for _, staticMainDomain := range rareDomainSuffix {
+		if staticMainDomain == targetDomain.Domain {
+			targetDomain.Domain = strs.Concat(split[length-3], ".", targetDomain.Domain)
+			break
+		}
+	}
+
+	index := strings.LastIndex(domain, targetDomain.Domain)
+	if index > 0 {
+		targetDomain.SubDomain = domain[:index-1]
+	}
+
+	return targetDomain, nil
 }
--- vutil/vhttp/http_test.go
+++ vutil/vhttp/http_test.go
@@ -1,7 +1,6 @@
 package vhttp
 
 import (
-	"fmt"
 	"log"
 	"testing"
 )
@@ -18,55 +17,4 @@ func TestURL(t *testing.T) {
 
 	d := IsURL("golang.org")
 	log.Print(d)
-}
-
-func TestDomain(t *testing.T) {
-	domName := "www.golang.org"
-
-	if !IsDomain(domName) {
-		fmt.Printf("Domain Name %s is invalid\n", domName)
-	} else {
-		fmt.Printf("Domain Name %s is VALID\n", domName)
-	}
-
-	domName = "www.socketloop,.com"
-
-	if !IsDomain(domName) {
-		fmt.Printf("Domain Name %s is invalid\n", domName)
-	} else {
-		fmt.Printf("Domain Name %s is VALID\n", domName)
-	}
-
-	domName = "subdomain-socketloop.com"
-
-	if !IsDomain(domName) {
-		fmt.Printf("Domain Name %s is invalid\n", domName)
-	} else {
-		fmt.Printf("Domain Name %s is VALID\n", domName)
-	}
-
-	domName = "-socketloop.com" // invalid starts with hyphen
-
-	if !IsDomain(domName) {
-		fmt.Printf("Domain Name %s is invalid\n", domName)
-	} else {
-		fmt.Printf("Domain Name %s is VALID\n", domName)
-	}
-
-	domName = "socketloop.co_" // invalid ends with underscore
-
-	if !IsDomain(domName) {
-		fmt.Printf("Domain Name %s is invalid\n", domName)
-	} else {
-		fmt.Printf("Domain Name %s is VALID\n", domName)
-	}
-
-	domName = "subdomain.socketloop.com"
-
-	if !IsDomain(domName) {
-		fmt.Printf("Domain Name %s is invalid\n", domName)
-	} else {
-		fmt.Printf("Domain Name %s is VALID\n", domName)
-	}
-
 }
