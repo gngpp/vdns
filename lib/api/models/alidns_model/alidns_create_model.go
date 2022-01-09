0affa0f7da938694ba32be90844fd787dd2d8eaa
zf1976 <1596863112@qq.com>
2022-01-09 20:03:35 +0800

    feat: update alidns_create_model.go

--- lib/api/models/alidns_model/alidns_create_model.go
+++ lib/api/models/alidns_model/alidns_create_model.go
@@ -2,11 +2,11 @@ package alidns_model
 
 import "vdns/util/vjson"
 
-type CreateDomainRecordResponseBody struct {
+type CreateDomainRecordResponse struct {
 	RequestId *string `json:"RequestId,omitempty" xml:"RequestId,omitempty"`
 	RecordId  *string `json:"RecordId,omitempty" xml:"RecordId,omitempty"`
 }
 
-func (s *CreateDomainRecordResponseBody) String() string {
+func (s *CreateDomainRecordResponse) String() string {
 	return vjson.PrettifyString(s)
 }
