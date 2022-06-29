bfc4d6f7f2eb557545dc37f6c439d9861d2fb780
remake <verticle@foxmail.com>
2022-06-29 11:54:55 +0800

    feat: update server command

--- server/server.go
+++ server/server.go
@@ -1,21 +1,45 @@
 package server
 
 import (
+	"fmt"
+	"time"
 	"vdns/lib/vlog"
 
 	"github.com/kardianos/service"
 )
 
-type Vdns struct{}
+type Vdns struct {
+	interval int
+}
+
+func NewVdns(interval int) Vdns {
+	if interval <= 0 {
+		return Vdns{
+			interval: 5,
+		}
+	}
+	return Vdns{
+		interval: interval,
+	}
+}
 
 func (p *Vdns) Start(s service.Service) error {
 	// Start should not block. Do the actual work async.
+	vlog.Debug(s)
 	go p.run()
 	return nil
 }
 func (p *Vdns) run() {
 	// Do work here
-	vlog.Info("vdns starts to execute the service")
+	vlog.Info("vdns start to execute the service")
+	vlog.Infof("interval execution time: %v minute", p.interval)
+	timer := time.NewTicker(time.Minute * time.Duration(p.interval))
+	for {
+		select {
+		case <-timer.C:
+			fmt.Println("do..")
+		}
+	}
 }
 func (p *Vdns) Stop(s service.Service) error {
 	// Stop should not block. Return with a few seconds.
--- terminal/server_command.go
+++ terminal/server_command.go
@@ -1,44 +1,73 @@
 package terminal
 
 import (
+	"fmt"
 	"github.com/kardianos/service"
 	"github.com/liushuochen/gotable"
 	"github.com/urfave/cli/v2"
 	"os"
 	"strings"
+	"vdns/lib/util/convert"
 	"vdns/lib/util/strs"
 	"vdns/lib/vlog"
 	"vdns/server"
 )
 
-var vdns = server.Vdns{}
-
 func ServerCommand() *cli.Command {
-	var controlAction = [6]string{"run", "start", "stop", "restart", "install", "uninstall"}
-	var subCommand = make([]*cli.Command, 6)
+	var controlAction = [6]string{"exec", "start", "stop", "restart", "install", "uninstall"}
+	var subCommandList = make([]*cli.Command, 6)
 	for index, c := range controlAction {
-		if c == "run" {
-			subCommand[index] = &cli.Command{
+		if c == "exec" {
+			subCommandList[index] = &cli.Command{
 				Name:  c,
-				Usage: "Run vdns server",
-				Action: func(_ *cli.Context) error {
-					return handleServer()
+				Usage: "Exec vdns server",
+				Flags: []cli.Flag{
+					&cli.IntFlag{
+						Name:    "interval",
+						Aliases: []string{"i"},
+						Usage:   "Interval execution time",
+						Value:   5,
+					},
+				},
+				Action: func(ctx *cli.Context) error {
+					i := ctx.Int("interval")
+					return handleServer(i)
 				},
 			}
 		} else {
-			subCommand[index] = &cli.Command{
-				Name:  c,
-				Usage: strFirstToUpper(c) + " vdns service",
-				Action: func(_ *cli.Context) error {
-					return handleServer()
-				},
+			if c == "install" {
+				subCommandList[index] = &cli.Command{
+					Name:  c,
+					Usage: strFirstToUpper(c) + " vdns service",
+					Flags: []cli.Flag{
+						&cli.IntFlag{
+							Name:    "interval",
+							Aliases: []string{"i"},
+							Usage:   "Interval execution time",
+							Value:   5,
+						},
+					},
+					Action: func(ctx *cli.Context) error {
+						i := ctx.Int("interval")
+						return handleServer(i)
+					},
+				}
+			} else {
+				subCommandList[index] = &cli.Command{
+					Name:  c,
+					Usage: strFirstToUpper(c) + " vdns service",
+					Action: func(_ *cli.Context) error {
+						// default time interval
+						return handleServer(-1)
+					},
+				}
 			}
 		}
 	}
 
 	table, err := gotable.Create("Usage")
 	if err != nil {
-		vlog.Fatalf("error creating")
+		vlog.Fatalf("creating error")
 	}
 	_ = table.AddRow([]string{"Service will install / un-install, start / stop, and run a program as a service (daemon)."})
 	_ = table.AddRow([]string{"Currently supports Windows XP+, Linux/(systemd | Upstart | SysV), and OSX/Launchd."})
@@ -46,22 +75,26 @@ func ServerCommand() *cli.Command {
 		Name:        "server",
 		Usage:       "Use vdns server (support DDNS)",
 		Description: table.String(),
-		Subcommands: subCommand,
+		Subcommands: subCommandList,
 	}
 }
 
-func handleServer() error {
+func handleServer(interval int) error {
 	cfg := &service.Config{
 		Name:        "vdns",
 		DisplayName: "vdns server",
 		Description: "This is an vdns Go service.",
+		Arguments:   []string{"server", "exec", "-i", convert.AsStringValue(interval)},
 	}
+
+	vdns := server.NewVdns(interval)
 	vdnsService, err := service.New(&vdns, cfg)
 	if err != nil {
 		return err
 	}
 	vlog.Debugf("running args: %v", os.Args)
-	if len(os.Args) == 3 && os.Args[2] != "run" {
+	if len(os.Args) >= 3 && os.Args[2] != "exec" {
+		fmt.Println(os.Args[2])
 		err = service.Control(vdnsService, os.Args[2])
 		if err != nil {
 			return err
