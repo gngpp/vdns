60f6d0167ad17d0fd6ce1083b83512cba0e9d49f
remake <verticle@foxmail.com>
2022-06-29 14:32:50 +0800

    update

--- build.sh
+++ build.sh
@@ -6,15 +6,15 @@ tar -czvf linux_amd64_vdns.tar.gz ./vdns && rm ./vdns
 
 CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -ldflags="-s -w" -o linux_386_vdns ./main.go
 mv ./linux_386_vdns ./vdns && upx ./vdns
-tar -czvf linux_386_vdns.tar.gz ./vdn && rm ./vdns
+tar -czvf linux_386_vdns.tar.gz ./vdns && rm ./vdns
 
 CGO_ENABLED=0 GOOS=freebsd GOARCH=386 go build -ldflags="-s -w" -o freebsd_386_vdns ./main.go
 mv ./freebsd_386_vdns ./vdns && upx ./vdns
-tar -czvf freebsd_386_vdns.tar.gz ./vdn && rm ./vdns
+tar -czvf freebsd_386_vdns.tar.gz ./vdns && rm ./vdns
 
 CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 go build -ldflags="-s -w" -o freebsd_amd64_vdns ./main.go
 mv ./freebsd_amd64_vdns ./vdns && upx ./vdns
-tar -czvf freebsd_amd64_vdns.tar.gz ./vdn && rm ./vdns
+tar -czvf freebsd_amd64_vdns.tar.gz ./vdns && rm ./vdns
 
 CGO_ENABLED=0 GOOS=freebsd GOARCH=arm go build -ldflags="-s -w" -o freebsd_arm64_vdns ./main.go
 mv ./freebsd_arm64_vdns ./vdns && upx ./vdns
