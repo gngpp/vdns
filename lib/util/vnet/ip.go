14fc98114dcb60dd77c67484f250ff8c6ccb4b24
remake <verticle@foxmail.com>
2022-06-30 21:57:32 +0800

    feat: add api

--- lib/util/vnet/ip.go
+++ lib/util/vnet/ip.go
@@ -13,6 +13,40 @@ const Ipv4Reg = `((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-
 
 const Ipv6Reg = `((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))`
 
+func GetIpv4AddrForName(card string) []string {
+	v4List, _, err := GetCardInterface()
+	if err != nil {
+		return []string{}
+	}
+	if v4List != nil {
+		for _, i := range v4List {
+			if i.Name == card && i.Ipv4() {
+				if len(i.Address) > 0 {
+					return i.Address
+				}
+			}
+		}
+	}
+	return []string{}
+}
+
+func GetIpv6AddrForName(card string) []string {
+	_, v6List, err := GetCardInterface()
+	if err != nil {
+		return []string{}
+	}
+	if v6List != nil {
+		for _, i := range v6List {
+			if i.Name == card && i.Ipv6() {
+				if len(i.Address) > 0 {
+					return i.Address
+				}
+			}
+		}
+	}
+	return []string{}
+}
+
 func GetIpv4AddrForUrl(url string) string {
 	return getIpAddr(Ipv4Reg, url)
 }
